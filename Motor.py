5d11d3628dbfebf0880b953d048533e233f245ec
tobulf <tobulf@gmail.com>
2017-08-10 19:25:23 +0200

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -1,11 +1,14 @@
 from microbit import*
 
 power = 0
+
 while True:
     while power < 1023:
-        pin6.write_analog(power)
         power += 1
-
+        pin0.write_analog(power)
+        sleep(10)
+        
     while power > 0:
-        pin6.write_analog(power)
-        power -=1
+        power -= 1
+        pin0.write_analog(power)
+        sleep(10)
